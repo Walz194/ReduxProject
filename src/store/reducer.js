24d5426b5ba31d5d11c719d8c01fae22b291a3f5
Olawale <olawale.akinodanye@gmail.com>
2020-11-30 12:37:31 +0100

    Before splitting the reducers

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,29 +1,49 @@
+import * as actionTypes from "./actions";
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action)=>{
-    if(action.type === 'INCREMENT'){
-        return{
-            counter: state.counter + 1
-        }
+    switch(action.type){
+        case actionTypes.INCREMENT:
+            console.log("Incremantality");
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
+        case actionTypes.DECREMENT:
+            console.log("Decremantality");
+            return{
+                ...state,
+                counter: state.counter - 1
+            }
+        case actionTypes.ADD:
+            return{
+                ...state,
+                counter: state.counter + action.val
+            }
+        case actionTypes.SUBTRACT:
+           return{
+               ...state,
+             counter: state.counter - action.val
+            }
+        case actionTypes.STORE_RESULT:
+            return{
+                ...state,
+                results: state.results.concat({id: new Date(),value: state.counter})
+            }
+        case actionTypes.DELETE_RESULT:
+            // const id = 2;
+            // const newArray = [...state.results];
+            // newArray.splice(id,1);
+            const updatedArray = state.results.filter(result=> result.id !== action.resultID)
+            return{
+                ...state,
+                results: updatedArray //newArray
+            }
     }
-    if(action.type === "DECREMENT"){
-        return{
-            counter: state.counter - 1
-        }
-    }
-    if(action.type === "ADD"){
-        return{
-            counter:state.counter + 10
-        }
-    }
-    if(action.type === "SUBTRACT"){
-        return{
-            counter: state.counter + 10
-        }
-    }
-    return state;
+    return state;    
 };
 
 export default reducer;
